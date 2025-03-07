b68b1f37ffbe8b42016c67138e24e451062ce47b
Priya Mohan <136183476+PriyaVajram@users.noreply.github.com>
2025-03-07 21:26:47 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Memory Game
+Once the cards are clicked, the timer starts to run.
+If the flipped card is matching the score will be will be increased or else the card will be un-flipped.
+If you want to start the game then can click restart button and the card position will be shuffled for every game.
+Once the restart is clicked, can play the game from beginning. 
+
---
+++ script.js
@@ -0,0 +1,102 @@
+let cards = document.querySelectorAll('.card_inner')
+let firstClick = false
+let cardPair = []
+
+cards.forEach((card)=>{
+    card.state = 'unclicked'
+})
+
+shuffle()
+
+for(let i=0; i<cards.length; i++){
+    cards[i].addEventListener('click',()=>{
+        if(!firstClick){time()}
+        firstClick = true        
+
+        if(cards[i].state == 'unclicked'){
+            cards[i].style.transform = 'rotateY(180deg)'
+            cards[i].state = 'clicked'
+            cardPair.push(cards[i])
+            check()
+        }
+
+        else if(cards[i].state == 'clicked'){
+            cards[i].style.transform = 'rotateY(0deg)'
+            cards[i].state = 'unclicked'
+            cardPair = []
+        }
+    })
+}
+
+function check(){
+    if(cardPair.length==2){
+        if(cardPair[0].querySelector('img').src==cardPair[1].querySelector('img').src){
+            matched()
+        }
+        else{
+            unmatched(cardPair[0],cardPair[1])
+        }
+    }  
+}
+
+function matched(){
+    cardPair[0].state='blocked'
+    cardPair[1].state='blocked'
+    cardPair = []
+    let score = document.querySelector('#score').innerHTML
+    score++
+    document.querySelector('#score').innerHTML = score
+}
+
+function unmatched(x,y){
+    setTimeout(()=>{
+        x.state = 'unclicked'
+        y.state = 'unclicked'
+        x.style.transform = "rotateY(0deg)"
+        y.style.transform = "rotateY(0deg)"
+    },750)
+    cardPair[0].state = 'blocked'
+    cardPair[1].state = 'blocked'
+    cardPair = []
+}
+
+function time(){
+    let secs = 0
+    let mins = 0
+    let SS
+    let MM
+    setInterval(()=>{
+        secs++
+        if(secs==60){secs=0; mins++}
+
+        secs<10?SS=`0${secs}`:SS=`${secs}`
+        mins<10?MM=`0${mins}`:SS=`${mins}`
+        
+        document.querySelector('#time').innerHTML = `${MM}:${SS}`
+    }, 1000)
+}
+
+function shuffle(){
+    let images = document.querySelectorAll('img')
+    let srcs = ['assets/angry.jpg','assets/cool.jpg','assets/foodie.jpg','assets/love.jpg','assets/silent.jpg','assets/smiley.jpg','assets/angry.jpg','assets/cool.jpg','assets/foodie.jpg','assets/love.jpg','assets/silent.jpg','assets/smiley.jpg']
+    
+    for(let i=srcs.length-1; i>0; i--){
+        let j = Math.floor(Math.random() * i)
+        let temp = srcs[i]
+        srcs[i] = srcs[j]
+        srcs[j] = temp
+      }
+      
+      for(let i=0; i<images.length; i++){
+          images[i].src = srcs[i]
+      }
+}
+function restartGame() {
+    score = 0;
+    document.getElementById('score').textContent = score;
+    window.location.reload();
+    shuffle();
+    
+    
+}
+document.getElementById('restart').addEventListener('click', restartGame);
